7e4d4b03d60bf49e0f4ca07f59145a485fac13c7
Hayk <hhahayastan@gmail.com>
2024-04-21 17:11:15 +0000

    info.sh information about network

--- info.sh
+++ info.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-while true; do
-    echo ""
-    echo "                            Disk usage in mb"
-    echo ""
-    free --mega
-    sleep 3s
-    echo "################################################################################"
 
-    echo "                            Disk usage in gb"
+list=$(find /sys/class/net -type l)
+for nic in $list
+do
+    echo "*** [ NIC: $nic ] ***"
+    cat "${nic}/uevent"
     echo ""
-    df -BG
-    sleep 3s
-    echo "################################################################################"
-
-    echo "                            Disk usage in %"
-    echo ""
-    df -h -x tmpfs --output=source,pcent
-    sleep 1m
 done
+
+# Retrieve the IP address
+ip_address=$(hostname -I | awk '{print $1}')
+
+# Display the IP address
+echo "IP Address: $ip_address"
+
+
+ip_address=$(ifconfig | grep 'inet ' | awk '{print $2}')
+echo $ip_address
+
+
+interface="lo"  # Replace with your desired interface name
+ip_addr=$(ip addr show "$interface" | awk '/inet / {print $2}')
+echo "IP address of $interface: $ip_addr"
+
+echo ""
+
+ifconfig | grep -w inet |grep -v 127.0.0.1| awk '{print $2}' | cut -d ":" -f 2
